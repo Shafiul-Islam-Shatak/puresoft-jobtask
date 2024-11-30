8c09ba19d9cc2ead09fd0c3ce4177188b1137f49
Shatak-the-imposter <helloshatak@gmail.com>
2024-11-30 22:54:24 +0600

    timeframeFilter done

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar'
+import { IoMdDownload } from "react-icons/io";
+import TimeframeFilter from '../components/Filter/TimeframeFilter';
+
 const page = () => {
     return (
-        <div>
+        <div className='flex'>
             <Navbar></Navbar>
+            <div className='flex-1 px-8 mt-4 '>
+                <div className='flex justify-between py-2 items-center '>
+                    <h1 className='text-2xl font-bold'>Reports</h1>
+                    <h1 className='flex items-center'><IoMdDownload/>Download</h1>
+                </div>
+                <hr className='mt-9'/>
+
+                <TimeframeFilter/>
+            </div>
         </div>
     );
 };
